14013ccff1092b9449dc5faf012c6301f03c4df1
carlyannrich <63616608+carlyannrich@users.noreply.github.com>
2021-12-02 14:46:17 +0000

    workflow settings and again

--- test-app/lighthouserc.js
+++ test-app/lighthouserc.js
@@ -2,6 +2,7 @@ module.exports = {
     ci: {
         collect: {
             startServerCommand: 'npm next start',
+            url: ['http://localhost:3000']
         },
         upload: {
             target: 'temporary-public-storage',
